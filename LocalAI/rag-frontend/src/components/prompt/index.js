d78701bcaabfc5da1871d91247b778689cb9cb04
Adobageedo <100688637+Adobageedo@users.noreply.github.com>
2025-05-28 18:00:36 +0200

    Google Oauth

--- LocalAI/rag-frontend/src/components/prompt/index.js
+++
@@ -1,1 +0,0 @@
-export { default as PromptForm } from './PromptForm';
--- LocalAI/rag-frontend/src/types/index.js
+++
@@ -1,6 +0,0 @@
-// Ce fichier facilite l'importation des types depuis d'autres fichiers
-export * from './auth';
-export * from './documents';
-export * from './nextcloud';
-export * from './email';
-export * from './ai';
