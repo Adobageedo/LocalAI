331e386399f10dcdfa08ba7180dc13e217edd299
Adobageedo <100688637+Adobageedo@users.noreply.github.com>
2025-11-05 17:12:48 +0100

    quickaction

--- frontend/src/components/features/email/EmailReader/QuickActions/InfoPDP.tsx
+++ frontend/src/components/features/email/EmailReader/QuickActions/InfoPDP.tsx
@@ -10,35 +10,45 @@ const InfoPDP: React.FC = () => {
       if (currentEmail) {
         const template = `
           Bonjour,
-          <br/><br/>
+          <br/>
           Dans le cadre de la préparation du plan de prévention (PDP) pour votre intervention sur le parc éolien, pourrais-tu me transmettre l'analyse de risque ainsi que le mode opératoire ?
           Nous aurions également besoin des coordonnées de vos intervenants sur site ainsi que de leurs habilitations.
-          <br/><br/>
+          <br/>
           De plus, merci de bien vouloir remplir le tableau ci-dessous et de me le renvoyer complété :
-          <br/><br/>
-          <table border="1" cellpadding="5" cellspacing="0">
+          <br/>
+            <table border="1" cellpadding="5" cellspacing="0" style="border-collapse: collapse;">
             <tr>
-              <th>Raison sociale</th>
-              <th>Adresse</th>
-              <th>Téléphone</th>
-              <th>Représentant légal</th>
-              <th>Représentant sur site</th>
-              <th>Téléphone</th>
-              <th>E-mail</th>
+                <th style="width: 200px; text-align: left; padding: 5px; background-color: #f2f2f2;">Raison sociale</th>
+                <td style="padding: 5px;"></td>
             </tr>
             <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+                <th style="text-align: left; padding: 5px; background-color: #f2f2f2;">Adresse</th>
+                <td style="padding: 5px;"></td>
             </tr>
-          </table>
-          <br/><br/>
+            <tr>
+                <th style="text-align: left; padding: 5px; background-color: #f2f2f2;">Téléphone</th>
+                <td style="padding: 5px;"></td>
+            </tr>
+            <tr>
+                <th style="text-align: left; padding: 5px; background-color: #f2f2f2;">Représentant légal</th>
+                <td style="padding: 5px;"></td>
+            </tr>
+            <tr>
+                <th style="text-align: left; padding: 5px; background-color: #f2f2f2;">Représentant sur site</th>
+                <td style="padding: 5px;"></td>
+            </tr>
+            <tr>
+                <th style="text-align: left; padding: 5px; background-color: #f2f2f2;">Téléphone</th>
+                <td style="padding: 5px;"></td>
+            </tr>
+            <tr>
+                <th style="text-align: left; padding: 5px; background-color: #f2f2f2;">E-mail</th>
+                <td style="padding: 5px;"></td>
+            </tr>
+            </table>
+          <br/>
           Merci d'avance pour ton retour.
-          <br/><br/>
+          <br/>
           Cordialement,
         `;
         await insertTemplate(template);
